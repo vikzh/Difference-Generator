8c40c555070e70671793ed45d8e427a7815bc0e6
reddowell <vikt.anmail@gmail.com>
2018-08-21 07:00:13 +0300

    lint fix

--- tests/gendiffTest.php
+++ tests/gendiffTest.php
@@ -15,7 +15,13 @@ class CalcDiffTest extends TestCase
 - proxy: 123.234.53.22
 + verbose: true
 DOC;
-        $this->assertEquals($jsonDiff, implode(PHP_EOL, \Differ\calcDiff('tests/data/before.json',
-            'tests/data/after.json', 'json')));
+        $this->assertEquals($jsonDiff, implode(
+            PHP_EOL,
+            \Differ\calcDiff(
+                'tests/data/before.json',
+                'tests/data/after.json',
+                'json'
+            )
+        ));
     }
 }
